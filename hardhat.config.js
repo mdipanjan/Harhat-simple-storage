c5f9a616839b8e09ba44e98ce4dfe719997d5422
mdipanjan <mdipanjan33@gmail.com>
2023-01-18 19:50:34 +0530

    Added script to verify with etherscan api after deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
 /** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_URL = process.env.RPC_URL;
+const GOERLI_PRIVATE_KEY = [process.env.PRIVATE_KEY];
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
     goerli: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: GOERLI_URL,
+      accounts: GOERLI_PRIVATE_KEY,
       chainId: 5,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {}
 
@@ -10,6 +10,27 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`simple storage deployed: ${simpleStorage.address}`);
+  // Verify contract deployment on etherscan
+  console.log(network.config);
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+}
+async function verify(contactAddress, args) {
+  console.log("verifying contract..");
+  try {
+    await run("verify:verify", {
+      address: contactAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified");
+    } else {
+      console.log(error, "Error");
+    }
+  }
 }
 main()
   .then(() => process.exit(0))
